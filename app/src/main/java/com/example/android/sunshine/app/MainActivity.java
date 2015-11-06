073582ff0261abab4ae3d0c2d154cb4b8067a90a
ChrisSullivan <cr_sullivan@hotmail.com>
2015-11-06 21:31:25 +1300

    Add github comment and push up to github

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -12,6 +12,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Chris Sullivan 06Nov 2015
+ * https://github.com/cr-sullivan/Sunshine-Version-2a
+ *
  */
 package com.example.android.sunshine.app;
 
